15640baf2b5a9f58d0248fd97d5d320d488b8265
austinjmp <90218555+austinjmp@users.noreply.github.com>
2023-12-07 12:03:25 -0700

    Added config to hardtime.lua, updated tab spacing to 4

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -3,5 +3,21 @@ return {
 	command = "Hardtime",
 	lazy = false,
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-	opts = {},
+	opts = {
+		hints = {
+			["k%^"] = {
+				message = function()
+					return "Use - instead of k^" -- return the hint message you want to display
+				end,
+				length = 2, -- the length of actual key strokes that matches this pattern
+			},
+			["d[tTfF].i"] = { -- this matches d + {t/T/f/F} + {any character} + i
+				message = function(keys) -- keys is a string of key strokes that matches the pattern
+					return "Use " .. "c" .. keys:sub(2, 3) .. " instead of " .. keys
+					-- example: Use ct( instead of dt(i
+				end,
+				length = 4,
+			},
+		},
+	},
 }
