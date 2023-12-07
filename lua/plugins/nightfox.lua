746b58813956d78ce665430989030f1b846f0255
austinjmp <90218555+austinjmp@users.noreply.github.com>
2023-12-07 09:16:47 -0700

    Initial Commit for NeoVim configuration

---
+++ lua/plugins/nightfox.lua
@@ -0,0 +1,8 @@
+return{
+	"EdenEast/nightfox.nvim", -- Github colourscheme url
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd('colorscheme nightfox') --Colorscheme name
+	end
+}
