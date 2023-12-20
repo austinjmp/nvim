73b53a7c3932ea3051a58585223ff5becbe40c7d
austinjmp <90218555+austinjmp@users.noreply.github.com>
2023-12-19 23:37:43 -0700

    fixed nvim-tree nav

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,15 +1,21 @@
+local keymap = vim.keymap
+
 return {
-	"nvim-tree/nvim-tree.lua",
-	lazy = false,
-	config = function()
-		vim.cmd([[hi NvimTreeNormal guibg=NONE ctermbg=None]])
-		require("nvim-tree").setup({
-			filters = {
-				dotfiles = false,
-			},
-			view = {
-				adaptive_size = true,
-			},
-		})
-	end,
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,
+    config = function()
+        vim.cmd([[hi NvimTreeNormal guibg=NONE ctermbg=None]])
+        require("nvim-tree").setup({
+            filters = {
+                dotfiles = false,
+            },
+            view = {
+                adaptive_size = true,
+            },
+        })
+    end,	
+    keys = {
+        keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>"),
+        keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>"),
+    },
 }
