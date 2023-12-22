63f4e54d27662a8662f9ba7f9f64a5fd279f6b42
austinjmp <90218555+austinjmp@users.noreply.github.com>
2023-12-22 09:46:02 -0700

    added lsp-zero, removed neodev,neoconf

---
+++ lua/plugins/lsp-zero.lua
@@ -0,0 +1,50 @@
+local config = function()
+    local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+    -- Auto-install lazy.nvim if not present
+    if not vim.loop.fs_stat(lazypath) then
+        print('Installing lazy.nvim....')
+        vim.fn.system({
+            'git',
+            'clone',
+            '--filter=blob:none',
+            'https://github.com/folke/lazy.nvim.git',
+            '--branch=stable', -- latest stable release
+            lazypath,
+        })
+    end
+
+    vim.opt.rtp:prepend(lazypath)
+
+    local lsp_zero = require('lsp-zero')
+
+    lsp_zero.on_attach(function(client, bufnr)
+        -- see :help lsp-zero-keybindings
+        -- to learn the available actions
+        lsp_zero.default_keymaps({buffer = bufnr})
+    end)
+
+    -- see :help lsp-zero-guide:integrate-with-mason-nvim
+    -- to learn how to use mason.nvim with lsp-zero
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+        handlers = {
+            lsp_zero.default_setup,
+        }
+    })
+end
+
+return {
+
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    lazy = false,
+    dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/nvim-cmp',
+        'L3MON4D3/LuaSnip',
+    },
+}
